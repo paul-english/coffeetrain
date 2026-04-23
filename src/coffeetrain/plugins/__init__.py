c32a0ffee3ed2099bf2e1f62f957c29e418eda92
Paul English <paulnglsh@gmail.com>
2026-04-23 14:36:35 -0600

    lr monitor plugin

--- src/coffeetrain/plugins/__init__.py
+++ src/coffeetrain/plugins/__init__.py
@@ -4,6 +4,7 @@ from coffeetrain.plugins.early_stopping import early_stopping
 from coffeetrain.plugins.cuda_accelerate import cuda_accelerate
 from coffeetrain.plugins.torch_compile import torch_compile
 from coffeetrain.plugins.comet import comet_plugin
+from coffeetrain.plugins.lr_monitor import lr_monitor_plugin
 
 default_plugins = [
     train_plugin,
